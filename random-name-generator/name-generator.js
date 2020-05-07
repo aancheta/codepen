e1a405ae4ec1aa2acd5af9b32621f3e9857413e4
AJ <ancheetah@users.noreply.github.com>
2020-05-06 21:25:37 -0400

    pseudocode

---
+++ random-name-generator/name-generator.js
@@ -0,0 +1,11 @@
+//List random words to choose from 
+const nouns = ["jelly", "glass", "lettuce", "turkey", "glove", "surprise", "plant", "asteroid", "galaxy", "nebula", "neutron", "proton", "electron", "flare", "comet", "corona"];
+const adj = ["boiled", "petite", "fresh", "fushcia", "ludicrous", "disastrous", "bewildered", "cloudy", "holistic", "glossy", "fluffy", "abiding"];
+const adv = ["keenly", "majestically", "overconfidently", "questionably", "brightly", "utterly", "simply", "almost", "fataly", "fiercely"];
+
+//Generate 3 random numbers
+    //For each array we need a whole number between 0 and array.length
+//Use the random numbers to pick one value from each array
+//Concatenate the three words with a dash "-"
+    //let username = `${}-${}-${}`;
+//Return value is a string of form {adverb}-{adjective}-{noun}
